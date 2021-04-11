eb9b88635cedcc47b26049c3576ae9325f8a1652
vejei <chengsnem@163.com>
2021-04-11 21:48:48 +0800

    Add Toolbar, TabLayout and ViewPager2

--- samples/src/main/java/io/github/vejei/progressindicator/samples/MainActivity.java
+++ samples/src/main/java/io/github/vejei/progressindicator/samples/MainActivity.java
@@ -1,8 +1,12 @@
 package io.github.vejei.progressindicator.samples;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+        ViewPager2 pager = findViewById(R.id.pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        pager.setAdapter(new PageAdapter(this));
+        new TabLayoutMediator(tabLayout, pager, (tab, position) -> {
+            if (position == 0) {
+                tab.setText("Indeterminate");
+            } else if (position == 1) {
+                tab.setText("Determinate");
+            }
+        }).attach();
     }
 }
